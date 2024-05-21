21790e9e168da7a37143644d4379d06269c2dd9a
Kath <33117050-00001100607602s@users.noreply.replit.com>
2024-05-21 13:25:19 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@s10-aula-2-1/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std; 
+
+//Resposta da Pergunta 1: 
+int main () { 
+  class Pessoa {
+  private: 
+  string nome; 
+  int idade;
+
+  public: 
+  string getnome ():
+  return nome; 
+  }
+int getidade (){
+  return idade:
+
+// Resposta da Pergunta 2:
+class Account {
+  private: 
+    int saldo;  
+
+  public:
+
+int getsaldo () {
+  retur saldo; 
+}
+
+void colocardinheiro() {
+  int dinheiro = 10; 
+cout << (dinheiro + getsaldo ()) << endl ; 
+}; 
+
+
+//Resposta da Pergunta 3:
+clss SuperTemperatura {
+  private: 
+    int temperatura; 
+} 
+
+  public: 
+  int gettemperatura () {
+    return temperatura; 
+  } 
+
+  void mudatemperatura (){ 
+  temperatura = 30;
+  } 
